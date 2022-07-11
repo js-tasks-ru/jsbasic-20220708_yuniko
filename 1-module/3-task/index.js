a7d40ce871e16f3978e95ae1712f09faf3b2c011
Yulia Nikonorova <ylia.nikonorova@yandex.ru>
2022-07-11 17:47:46 +0300

    Перевод на заглавную букву

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0){
+    return('');
+  }
+  else{
+    return(str.charAt(0).toUpperCase() + str.slice(1));     
+  }
 }
+ucFirst('юлия') === 'Юлия';
+ucFirst('в') === 'В';
+ucFirst('') === '';
